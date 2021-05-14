aba5dfce548befaa38009b08dc7d06bf1a135f1a
ElaineHuang1100 <83899775+ElaineHuang1100@users.noreply.github.com>
2021-05-14 21:36:27 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -5,13 +5,13 @@ var game=document.createElement("table");
 var stat=0;//0:rules, 1:start, 2:end
 var Ans='';//player answer
 var ans=0;//correct answer
-var Answer=document.getElementById("Answer");
+var Answer=document.getElementById('Answer');
 var yourAns=document.querySelector('.yourAns');
 var score=0,fscore=0;//AC WA
 var t=0,T,Ttime=60;//total time (in sec)
 var minn=6,maxn=10;//食物最少項數、最多項數
 var minp=69,maxp=119;//價格
-var minq=8,maxq=10;//數量
+var maxq=10;//數量
 //選擇難度
 var mode=document.querySelector('.mode');
 var Mode=Number(mode.value);
@@ -50,6 +50,7 @@ function start()
 function end()
 {
 	while(game.rows.length) game.deleteRow(0);
+	Ans='';
 	stat=2;
 	Answer.value="reset";
 	Answer.type="hidden";
@@ -68,12 +69,12 @@ function genProb()//generate problem
 	//set the problem and answer here
 	while(game.rows.length) game.deleteRow(0);
 	game.align='center';
-	if(Mode>1) game.innerHTML="外加10%服務費";//
-	var food=Math.floor(Math.random()*(maxn-minn+1)+minn);
+	if(Mode==2) game.innerHTML="外加10%服務費";//
+	//var food=Math.floor(Math.random()*(maxn-minn+1)+minn);
 	//var drink=Math.floor(Math.random()*(maxn-minn+1)+minn);
 	var th=game.insertRow();
 	var header=['編號','原價','數量','優惠'];
-	for(var i=0;i<(Mode==1?3:4);i++)
+	for(var i=0;i<4;i++)
 	{
 		var headerCell=document.createElement("th");
 		headerCell.innerHTML=header[i];
@@ -81,26 +82,37 @@ function genProb()//generate problem
 	}
 	//create the table of food and drinks
 	ans=0;
-	for(var i=0;i<food;i++)
+	var food=[0,0,0,0,0,0,0,0,0,0];
+	for(var i=0;i<maxq;i++)
 	{
+		var f=Math.floor(Math.random()*10+0);
+		food[f]++;
+	}
+	var index=0;
+	for(var i=0;i<maxq;i++)
+	{
+		if(!food[i]) continue;
+		index++;
 		var row=game.insertRow();
 		//編號
 		var node=row.insertCell();
-		node.innerHTML=(i+1);
+		node.innerHTML=(index);
 		//原價
 		node=row.insertCell();
 		var price=Math.floor(Math.random()*(maxp-minp+1)+minp);
 		node.innerHTML=price;
 		//數量
 		node=row.insertCell();
-		var quantity=Math.floor(Math.random()*(maxq-minq+1)+minq);
+		var quantity=food[i];
 		node.innerHTML=quantity;
-		ans+=price*quantity;
 		//優惠
+		node=row.insertCell();
 		if(Mode>1)
 		{
 			
 		}
+		else node.innerHTML="無";
+		ans+=price*quantity;
 	}
 	/*
 	<tr>
@@ -109,12 +121,6 @@ function genProb()//generate problem
 		<th>數量</th>
 		<th>優惠</th>
 	</tr>
-	<tr>
-		<td>食物A</td>
-		<td>40</td>
-		<td>5</td>
-		<td>無</td>
-	</tr>
 	*/
 	document.getElementById("game").appendChild(game);
 }
@@ -122,7 +128,7 @@ function upd()//update
 {
 	Score.innerHTML='AC: '+String(score)+', WA: '+String(fscore);
 	yourAns.textContent='Your ans = '+Ans;//just for testing the result
-	rule.textContent='按T送出答案';
+	rule.textContent='按P送出答案';
 }
 function tupd()//time update
 {
@@ -141,7 +147,7 @@ document.addEventListener('keydown', function(evt)
 		return;
 	}
 	//submit answer
-	if(evt.code=='KeyT'&&stat==1)
+	if(evt.code=='KeyP'&&stat==1)
 	{
 		inputAns();
 		checkAns();
